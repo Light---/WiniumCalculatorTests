6351beef87c8a39d398d4c972d4e8df5912e2c5c
Emma Leary <eleary01@qub.ac.uk>
2016-06-14 01:32:44 +0100

    Add basic calculator tests

---
+++ WiniumCalculatorTests/BaseTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace WiniumCalculatorTests
+{
+    public class BaseTest
+    {
+        public RemoteWebDriver Driver { get; set; }
+        public IWebElement MainWindow { get; set; }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var driverCapabilities = new DesiredCapabilities();
+            driverCapabilities.SetCapability("app", @"C:\Users\dev\Desktop\MyCalculator\MyCalculator\bin\Release\MyCalculator.exe");
+            Driver = new RemoteWebDriver(new Uri("http://localhost:9999"), driverCapabilities);
+
+            MainWindow = Driver.FindElementById("Form1");
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Driver.Close();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://github.com/2gis/Winium.Cruciatus/blob/master/tools/UISpy/UISpy.exe
